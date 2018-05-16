b7608abc764ece36a4ef011802fb77ce3d6192c5
jmoypace <jmoypace@gmail.com>
2018-05-16 03:13:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -29,6 +29,13 @@ $('#typing').on('keydown', function(key) {
 $(document).ready(function(){
 
 // call functions here
-
+$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 });
 }
