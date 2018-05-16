d714e7f8a7996a8c474a1a8705b771ab0f91a351
jmoypace <jmoypace@gmail.com>
2018-05-16 03:13:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 // call functions here
 $("form").on("submit", function() {
   if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
+    alert('Your form is going to be submitted now');
     return;
   }
   alert("you entered the wrong value");
