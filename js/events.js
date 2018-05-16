e15aff89d8c348ca9349d700e78c9302ea8b9b48
jmoypace <jmoypace@gmail.com>
2018-05-16 01:52:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 
 // call functions here
-
+//should bind a click event to the p tag. When the paragraph is clicked, the function should //alert "Hey!".
 });
 }
  function frameIt(){
